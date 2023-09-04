383de3a10910741a4163389f81fa29392850892c
The Humanist <42555733+TheHumanistX@users.noreply.github.com>
2023-09-04 12:57:11 -0400

    Split apart functions and useEffects in EthersContext for better ability to deal with asynchronous setting of state variables that other functions depend on

--- src/context/EthersContext.js
+++ src/context/EthersContext.js
@@ -4,7 +4,11 @@ import abi from '../abi/abi.json'
 
 const EthersContext = createContext();
 
-export const EthersProvider = ({ children, setIsLoading  }) => {
+export const EthersProvider = ({ children, setIsLoading }) => {
+
+    const [triggerUpdate, setTriggerUpdate] = useState(true);
+    const [isProviderReady, setIsProviderReady] = useState(false);
+    const [isTokenContractReady, setIsTokenContractReady] = useState(false);
 
     const [provider, setProvider] = useState(null)
     const [signer, setSigner] = useState(null)
@@ -19,122 +23,138 @@ export const EthersProvider = ({ children, setIsLoading  }) => {
     const contractAddress = '0xFB29697113015019c42E90fdBC94d9B4898D2602'
     const ETH_NULL_ADDRESS = '0x0000000000000000000000000000000000000000'
 
-    useEffect(() => {
+    const updateProviderAndNetwork = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        console.log('EthersContext - provider: ', provider)
+        const network = await provider?.getNetwork();
+        console.log('EthersContext - network: ', network)
+        setProvider(provider)
+        setNetwork(network)
+        setChainName(network ? network.name : null)
+    }
 
-        const ethersDataSetup = async () => {
-            try {
-                if (window.ethereum) {
-                    console.log('EthersContext - Entered try block in ethersDataSetup')
-                    const provider = new ethers.providers.Web3Provider(window.ethereum)
-                    setProvider(provider)
-                    console.log('EthersContext - provider: ', provider)
-                    const network = await provider.getNetwork()
-                    console.log('EthersContext - network: ', network)
-                    setNetwork(network)
-                    setChainName(network ? network.name : null)
-                    
-                    if (network?.chainId === 5) { // Check for Goerli network
-                        try {
-                            const signer = provider.getSigner()
-                            setSigner(signer)
-                            console.log('EthersContext - Entered second try block in ethersDataSetup, right before setting up wallet.')
-                            const tokenContract = new ethers.Contract(contractAddress, abi, signer)
-                            const walletAddress = await signer?.getAddress()
-                            console.log('EthersContext - walletAddress: ', walletAddress)
-                            console.log('EthersContext - tokenContract: ', tokenContract)
-                            setWalletAddress(walletAddress)
-                            setTokenContract(tokenContract)
-                            const decimals = await tokenContract?.decimals()
-                            const tokenBalance = await tokenContract?.balanceOf(walletAddress)
-                            console.log('EthersContext - tokenBalance: ', tokenBalance)
-                            const canMint = await tokenContract?.checkIfUserCanMint(walletAddress)
-                            const formattedBalance = ethers.utils.formatUnits(tokenBalance, decimals)
-                            setDecimals(decimals)
-                            setTokenBalance(tokenBalance)
-                            setCanMint(canMint)
-                            setFormattedTokenBalance(formattedBalance)
-                            setIsLoading(false)
-                        } catch (err) {
-                            if (err.code === 4001) {
-                                // User rejected request
-                                console.log("User rejected request")
-                            } else {
-                                console.error(err)
-                            }
-                            setIsLoading(false)
-                        }
+    const updateSignerAccountAndTokenContract = async (provider) => {
+        const signer = provider.getSigner()
+        const walletAddress = await signer.getAddress()
+        const tokenContract = new ethers.Contract(contractAddress, abi, signer)
+        console.log('EthersContext - walletAddress: ', walletAddress)
+        console.log('EthersContext - tokenContract: ', tokenContract)
+        setSigner(signer)
+        setWalletAddress(walletAddress)
+        setTokenContract(tokenContract)
+    };
+
+    const updateTokenData = async (tokenContract, walletAddress) => {
+        const decimals = await tokenContract?.decimals()
+        const tokenBalance = await tokenContract?.balanceOf(walletAddress)
+        console.log('EthersContext - tokenBalance: ', tokenBalance)
+        const canMint = await tokenContract?.checkIfUserCanMint(walletAddress)
+        const formattedBalance = ethers.utils.formatUnits(tokenBalance, decimals)
+        setDecimals(decimals)
+        setTokenBalance(tokenBalance)
+        setCanMint(canMint)
+        setFormattedTokenBalance(formattedBalance)
+        setIsLoading(false)
+    }
+
+    const handleChainChange = async (networkIdHex) => {
+        const networkId = parseInt(networkIdHex, 16)
+
+        if ( networkId === 5 ) {
+            setTriggerUpdate(!triggerUpdate);
+            setIsProviderReady(false);
+            setIsTokenContractReady(false);
+            window.location.reload()
+        } else {
+            alert('Please switch to the Goerli network!')
+        }
+
+    };
+
+    const handleAccountsChanged = async (accounts) => {
+        console.log('EthersContext accountsChanged entered on account change...')
+        if (accounts.length === 0) {
+            console.log('Please connect to MetaMask.');
+            alert('Your MetaMask is not connected anymore. Please unlock or reconnect.'); // display an alert
+            // handle account disconnection...
+            setWalletAddress(null);
+            setSigner(null);
+        } else if (accounts[0] !== walletAddress) {
+            setTriggerUpdate(!triggerUpdate);
+            setIsProviderReady(false);
+            setIsTokenContractReady(false);
+        }
+    };
+
+    useEffect(() => {
+        window.ethereum.on('chainChanged', handleChainChange);
+        window.ethereum.on('accountsChanged', handleAccountsChanged);
+      
+        // Cleanup
+        return () => {
+          window.ethereum.removeListener('chainChanged', handleChainChange);
+          window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+        };
+      }, []);
+
+    useEffect(() => {
+        if( window.ethereum ) {
+            const setupProviderAndNetwork = async () => {
+                try {
+                    await updateProviderAndNetwork()
+                    setIsProviderReady(true);
+                } catch (err) {
+                    if (err.code === 4001) {
+                        // User rejected request
+                        console.log("User rejected request")
+                    } else {
+                        console.error(err)
                     }
                 }
+            }
+            setupProviderAndNetwork()
+        }
+    }, [triggerUpdate])
+
+    useEffect(() => {
+
+        if (isProviderReady) { 
+        const ethersDataSetup = async () => {
+            try {
+                console.log('EthersContext - Entered second try block in ethersDataSetup, right before setting up wallet.')
+                await updateSignerAccountAndTokenContract(provider)
+                setIsTokenContractReady(true);
             } catch (err) {
-                if (err.message.includes('unknown account #0')) {
-                    // Handle error due to MetaMask being locked
-                    console.log('Metamask is locked. Please unlock and reconnect.')
-                    setWalletAddress(null)
-                    setSigner(null)
-                    setProvider(null)
+                if (err.code === 4001) {
+                    // User rejected request
+                    console.log("User rejected request")
                 } else {
-                    console.error('Error in EthersContext: ', err.message)
+                    console.error(err)
                 }
                 setIsLoading(false)
             }
-        };
+        }
+        ethersDataSetup()
+    }
+    }, [isProviderReady])
 
-        const handleChainChange = (networkIdHex) => {
-            const networkId = parseInt(networkIdHex, 16)
-            ethersDataSetup(networkId)
-            window.location.reload()
-        };
-
-         const handleAccountsChanged = async (accounts) => {
-            console.log('EthersContext accountsChanged entered on account change...')
-            if (accounts.length === 0) {
-                console.log('Please connect to MetaMask.');
-                alert('Your MetaMask is not connected anymore. Please unlock or reconnect.'); // display an alert
-                // handle account disconnection...
-                setWalletAddress(null);
-                setSigner(null);
-            } else if (accounts[0] !== walletAddress) {
-                const provider = new ethers.providers.Web3Provider(window.ethereum);
-                console.log('EthersContext accountsChanged provider: ', provider)
+    useEffect(() => {
+        if (isTokenContractReady) {
+            const ethersDataSetup = async () => {
                 try {
-                    const signer = await provider.getSigner();
-                    const walletAddress = await signer.getAddress();
-                    console.log('EthersContext walletAddress updated to: ', walletAddress)
-                    setSigner(signer);
-                    setWalletAddress(walletAddress);
-                    // window.location.reload()
-                } catch (error) {
-                    if (error.code === 4001) {
+                    await updateTokenData(tokenContract, walletAddress)
+                } catch (err) {
+                    if (err.code === 4001) {
                         // User rejected request
-                        console.log("User rejected request");
-                        // Add some user-friendly notification logic here
+                        console.log("User rejected request")
                     } else {
-                        console.error(error);
-                        alert('Error when getting wallet address. Please check your MetaMask connection.');
-                        setWalletAddress(null);
-                        setSigner(null);
+                        console.error(err)
                     }
                 }
-
             }
-        };
-
-        window.ethereum.on('chainChanged', handleChainChange)
-        window.ethereum.on('accountsChanged', handleAccountsChanged)
-
-        // Initial setup
-        ethersDataSetup().then(() => {
-            setIsLoading(false)  // Set isLoading to false when everything is initialized
-          });
-
-        // Clean up function
-        return () => {
-            window.ethereum.removeListener('chainChanged', handleChainChange)
-            window.ethereum.removeListener('accountsChanged', handleAccountsChanged)
-        };
-    }, []);
-
-
+            ethersDataSetup()
+        }
+    }, [isTokenContractReady])
 
 
     return (
